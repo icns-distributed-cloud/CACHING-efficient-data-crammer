5a77fe9c6137885c0380232529941cb4a4ab5e9d
Yunkon(Alvin) Kim <hermitkim1@gmail.com>
2019-11-18 16:37:24 +0900

    Add cache capacity auto-scaling function
    - Change a variable, self.list_size to slef._list_size in CircularList.py
    - Change gains for P, I, and D
    - Add variables and algorithms for cache capacity auto-scaling
    - Modify variables and algorithm in PID controller

--- CircularList.py
+++ CircularList.py
@@ -34,10 +34,11 @@ class CircularList:
     def __init__(self, list_size):
         self.__list = [None] * list_size
         self.__index = 0
+        self.__list_size = list_size
 
     def put(self, data):
         self.__list[self.__index] = data
-        self.__index = (self.__index + 1) % self.list_size
+        self.__index = (self.__index + 1) % self.__list_size
 
     @property
     def list(self):
